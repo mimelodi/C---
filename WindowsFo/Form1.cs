6fc5c430e522e41c0d47e94f83dcb7809a502993
mimelodi <bjjwon1@naver.com>
2024-05-29 19:33:34 +0900

    Update Form1.cs
    b

--- WindowsFo/Form1.cs
+++ WindowsFo/Form1.cs
@@ -20,6 +20,7 @@ namespace WindowsFo
         private void Form1_Load(object sender, EventArgs e)
         {
             Console.WriteLine("bye bye");
+            Console.WriteLine("thank you");
         }
     }
 }
